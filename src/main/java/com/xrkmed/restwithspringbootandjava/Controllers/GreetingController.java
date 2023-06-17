c6d47daf75038be7cbae93271db95cde0409ff89
João Guilherme M. Martins <114001978+xrkmed@users.noreply.github.com>
2023-06-17 02:49:04 -0300

    adicionado /sum e exceptionhandler

--- src/main/java/com/xrkmed/restwithspringbootandjava/Controllers/GreetingController.java
+++ src/main/java/com/xrkmed/restwithspringbootandjava/Controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.xrkmed.restwithspringbootandjava.Controllers;
 
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.xrkmed.restwithspringbootandjava.Greeting;
 
 @RestController
+@RequestMapping("/v1")
 public class GreetingController {
 
 	  private static final String template = "Hello, %s!";
 	  private static AtomicLong counter = new AtomicLong();
 	  
 	  @RequestMapping("/greeting")
-	  public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name) {
-		  return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	  public HashMap<Object, Object> greeting(@RequestParam(value="name", defaultValue = "World") String name) {
+		  HashMap<Object, Object> _result = new HashMap();
+		  Greeting greeting = new Greeting(counter.getAndIncrement(), String.format(template, name));
+		  _result.put("id", greeting.getId());
+		  _result.put("content", greeting.getContent());
+		  _result.put("apiVersion", "v1");
+		  return _result;
 	  }
 	
 }
