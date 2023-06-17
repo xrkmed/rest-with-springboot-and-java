12cfc6e8b952f0444f4dc0803c622c41e6838779
João Guilherme M. Martins <114001978+xrkmed@users.noreply.github.com>
2023-06-17 01:03:18 -0300

    Hello, %s! :)

---
+++ src/main/java/com/xrkmed/restwithspringbootandjava/Controllers/GreetingController.java
@@ -0,0 +1,22 @@
+package com.xrkmed.restwithspringbootandjava.Controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xrkmed.restwithspringbootandjava.Greeting;
+
+@RestController
+public class GreetingController {
+
+	  private static final String template = "Hello, %s!";
+	  private static AtomicLong counter = new AtomicLong();
+	  
+	  @RequestMapping("/greeting")
+	  public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name) {
+		  return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	  }
+	
+}
---
+++ src/test/java/com/xrkmed/restwithspringbootandjava/StartupTests.java
@@ -0,0 +1,13 @@
+package com.xrkmed.restwithspringbootandjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
